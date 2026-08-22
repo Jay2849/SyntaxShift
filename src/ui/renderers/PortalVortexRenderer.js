a287f52589e0a1b2fdf8d48fd1ba32195cefbb3d
Jay2849 <jayn75009@gmail.com>
2026-08-22 10:48:45 +0530

    feat(ui): add PortalVortexRenderer goal portal particle arc renderer

---
+++ src/ui/renderers/PortalVortexRenderer.js
@@ -0,0 +1,11 @@
+export function renderPortalVortex(ctx, portalBody, angle) {
+  if (!portalBody) return;
+  const { x, y } = portalBody.position;
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.strokeStyle = '#00f3ff';
+  ctx.beginPath();
+  ctx.arc(0, 0, 34, angle, angle + Math.PI * 1.3);
+  ctx.stroke();
+  ctx.restore();
+}
