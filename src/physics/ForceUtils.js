0699e36fb6aeee2653e01f0c2e96f732c55593fc
Jay2849 <jayn75009@gmail.com>
2026-08-22 11:16:15 +0530

    refactor(physics): add scaleForceByMass vector helper

--- src/physics/ForceUtils.js
+++ src/physics/ForceUtils.js
@@ -4,3 +4,5 @@
 export function calculateCounterForce(mass, gravityY, gravityX = 0) {
   return { x: -mass * gravityX, y: -mass * gravityY };
 }
+
+export function scaleForceByMass(force, mass) { return { x: force.x * mass, y: force.y * mass }; }
