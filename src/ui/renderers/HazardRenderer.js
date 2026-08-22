fbca554f1e32c4766f1531ce977cdf205a79c006
Jay2849 <jayn75009@gmail.com>
2026-08-22 10:51:15 +0530

    feat(ui): add HazardRenderer laser beam and spike hazard renderer

---
+++ src/ui/renderers/HazardRenderer.js
@@ -0,0 +1,7 @@
+export function renderHazards(ctx, lasers, spikes) {
+  lasers.forEach(laser => {
+    const b = laser.bounds;
+    ctx.fillStyle = '#ff0055';
+    ctx.fillRect(b.min.x, b.min.y, b.max.x - b.min.x, b.max.y - b.min.y);
+  });
+}
