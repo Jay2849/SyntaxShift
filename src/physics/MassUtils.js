18bed9198a6e3dd4c55e758bcbce33e09f098202
Jay2849 <jayn75009@gmail.com>
2026-08-22 11:13:45 +0530

    refactor(physics): add getDensityMultiplier mass helper

--- src/physics/MassUtils.js
+++ src/physics/MassUtils.js
@@ -4,3 +4,5 @@
 export function calculateBuoyantMass(bodyMass, gravityScale = 0.001) {
   return bodyMass * gravityScale * 1000;
 }
+
+export function getDensityMultiplier(isHeavy) { return isHeavy ? 0.005 : 0.001; }
