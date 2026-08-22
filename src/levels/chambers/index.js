f5b1cce2fd77f215126494023ce7f1ae785a72dd
Jay2849 <jayn75009@gmail.com>
2026-08-22 10:30:00 +0530

    feat(levels): add chambers registry index exporter

---
+++ src/levels/chambers/index.js
@@ -0,0 +1,7 @@
+import { Chamber1 } from './Chamber1_InversionTutorial.js';
+import { Chamber2 } from './Chamber2_SelectiveMass.js';
+import { Chamber3 } from './Chamber3_GravitationalChasm.js';
+import { Chamber4 } from './Chamber4_KineticPendulum.js';
+import { Chamber5 } from './Chamber5_LaserLabyrinth.js';
+
+export const CHAMBERS = [Chamber1, Chamber2, Chamber3, Chamber4, Chamber5];
